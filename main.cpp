6b8f507afb7a49de694e89b0022c12e6378ff8e8
kimbh <kimbh0437@gmail.com>
2026-03-22 23:58:33 +0900

    add shapes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "raylib.h"
+#include "tetray.hpp"
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -8,6 +9,23 @@ const char *TITLE = "Tetray";
 int main()
 {
     InitWindow(WIDTH, HEIGHT, TITLE);
+
+    for (int i = 0; i < tet::SHAPE_COUNT; i++)
+    {
+        for (int j = 0; j < tet::SHAPE_SIZE; j++)
+        {
+            for (int k = 0; k < tet::SHAPE_ROTATE_COUNT; k++)
+            {
+                for (int l = tet::SHAPE_SIZE - 1; l >= 0; l--)
+                {
+                    std::cout << ((tet::shapes[i][k][j] >> l) & 1 ? "#" : ".");
+                }
+                std::cout << '\t';
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "-----------------------------------------------------" << std::endl;
+    }
 
     while (!WindowShouldClose())
     {
