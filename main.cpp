62bdc9fe8c57e985c6d75cbe98e5ba5badf80761
kimbh <kimbh0437@gmail.com>
2026-05-03 16:56:24 +0900

    [feat] Add display block size constant

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ const int WIDTH = 800;
 const int HEIGHT = 600;
 const char *TITLE = "Tetray";
 
+const int BLOCK_SIZE = 20;
+
 int main()
 {
     InitWindow(WIDTH, HEIGHT, TITLE);
@@ -15,19 +17,19 @@ int main()
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
+        {
         ClearBackground(BLACK);
 
-        DrawLine(0, tet::BOARD_DEPTH * 10, tet::BOARD_WIDTH * 10, tet::BOARD_DEPTH * 10, WHITE);
+            DrawLine(0, tet::BOARD_DEPTH * BLOCK_SIZE, tet::BOARD_WIDTH * BLOCK_SIZE, tet::BOARD_DEPTH * BLOCK_SIZE, WHITE);
 
         for (byte r = 0; r < tet::BOARD_DEPTH; r++)
         {
             for (byte c = 0; c < tet::BOARD_WIDTH; c++)
             {
-                DrawRectangle(c * 10, r * 10, 10, 10, tet::GetColor(r, c));
+                    DrawRectangle(c * BLOCK_SIZE, r * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE, tet::GetColor(r, c));
+                }
             }
         }
-
         EndDrawing();
     }
 
